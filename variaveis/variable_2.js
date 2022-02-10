d9680c77275e351363c9e4c9235fec51a08b34a4
Felipe Moura <felipemimoura@gmail.com>
2022-02-10 11:12:01 -0300

    feat: desenvolvem exemplos executados na aula

---
+++ variaveis/variable_2.js
@@ -0,0 +1,13 @@
+// Declaraçao e atribuição da variável
+let pi = 3.141592;
+console.log(pi);
+
+// Reatribuição da variável
+pi = 3;
+console.log(pi);
+
+//respeitando o contexto de bloco
+if (true) {
+  let idade = 10;
+}
+console.log(idade)
