7eb37143adc7a6b36f9c6c863b0e460873c1c781
Felipe Moura <felipemimoura@gmail.com>
2022-02-26 17:54:39 -0300

    feat: realização dos exemplos sobre os tipos de dados

---
+++ data_type/data_type_01.js
@@ -0,0 +1,7 @@
+//Dados primitivos do javascript
+console.log(typeof 10);
+console.log(typeof 'javascript');
+console.log(typeof true);
+console.log(typeof Symbol());
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ data_type/data_type_02.js
@@ -0,0 +1,4 @@
+console.log((10).toFixed(2));
+console.log('Javascript'.replace('a', 4));
+console.log(true.toString());
+console.log((Symbol('Iterator')).toString());
